2f3ece6d7e17eccd8ca8bb394787e42e9a95a9c4
Pragya Jaiswal <pragya.jswl@gmail.com>
2015-07-03 12:01:11 +0530

    Search PP directory recursively.

--- protein_pilot vs iTRAQ/compare_pp_vs_itraqanalyzer.pl
+++ protein_pilot vs iTRAQ/compare_pp_vs_itraqanalyzer.pl
@@ -6,19 +6,16 @@ my $count = 0;
 my $pp_dir = 'D:\\ICGEB LAB\\Benchmarking of Quantwiz\\protein_pilot_results\\';
 my $itraq_dir = 'D:\\ICGEB LAB\\Benchmarking of Quantwiz\\iTRAQ_consensus_xml\\output\\';
 
-my @files_in_itraqdir = <$itraq_dir/*>;
-my $tot_files_in_itraq = @files_in_itraqdir;
-print "Total files in iTRAQ folder: $tot_files_in_itraq";
-my @files_in_ppdir = <$pp_dir/*>;
-my $tot_files_in_pp = @files_in_ppdir;
-print "Total files in PP folder: $tot_files_in_pp";
-
 opendir (PP_DIR, $pp_dir) or die "Cannot open directory $pp_dir: $!";
 opendir (iTRAQ_DIR, $itraq_dir) or die "Cannot open directory $itraq_dir: $!";
 
 my $itraq_count = 0;
+my $pp_count = 0;
+my $pp_total_done = 0;
+
+my @pp_files = readdir(PP_DIR);
+
 while (my $itraq_file = readdir(iTRAQ_DIR)) {
-	my $pp_count = 0;
 	if (index($itraq_file, 'tsv') != -1) {
 		$itraq_count += 1;
 		print "iTRAQ file: $itraq_file\n";
@@ -26,9 +23,12 @@ while (my $itraq_file = readdir(iTRAQ_DIR)) {
 		$filename =~ s/\.tsv//;
 		print "iTRAQ filename: $filename\n";
 		
-		while (my $pp_file = readdir(PP_DIR)) {
-			# print "Protein Pilot file: $pp_file\n";
-			$pp_count += 1;
+		# while (my $pp_file = readdir(PP_DIR)) {
+		foreach my $pp_file (@pp_files) {
+			# print "Protein Pilot file: $pp_file\n";			
+			$pp_total_done += 1;
+			# print "Number of times PP files encountered: $pp_total_done\n";
+
 			if (index($pp_file, '_SpectrumSummary.txt') == -1) {
 				print "Not the proper file format. File not parsed.\n";
 				next;
@@ -36,18 +36,20 @@ while (my $itraq_file = readdir(iTRAQ_DIR)) {
 
 			my $comp = $pp_file;
 			$comp =~ s/\_SpectrumSummary.txt//;
-			print "Protein Pilot filename: $comp\n";
+			# print "Protein Pilot filename: $comp\n";
+			
 			# if ($pp_count == 5) {
 			# 	print "Ending.";
 			# 	last;
 			# }
 			if ($filename eq $comp) {
 				print "Same files found.\n";
+				$pp_count += 1;
 				compare($pp_file, $itraq_file);
+				print "Number of Protein Pilot files processed: $pp_count of 45.\n";
 				last;
 			}
 			else {
-				print "Here.\n";
 				next;
 			}
 		}
@@ -57,7 +59,3 @@ while (my $itraq_file = readdir(iTRAQ_DIR)) {
 
 closedir(PP_DIR);
 closedir(iTRAQ_DIR);
-
-sub compare {
-	my ($file_a, $file_b) = @_;
-}
