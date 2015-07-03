d239514022912a709b1538d3de34e23c52be8e7b
Pragya Jaiswal <pragya.jswl@gmail.com>
2015-07-03 10:55:09 +0530

    Working code for comparing filenames in different directories.

--- protein_pilot vs iTRAQ/compare_pp_vs_itraqanalyzer.pl
+++ protein_pilot vs iTRAQ/compare_pp_vs_itraqanalyzer.pl
@@ -9,27 +9,38 @@ my $itraq_dir = 'D:\\ICGEB LAB\\Benchmarking of Quantwiz\\iTRAQ_consensus_xml\\o
 opendir (PP_DIR, $pp_dir) or die "Cannot open directory $pp_dir: $!";
 opendir (iTRAQ_DIR, $itraq_dir) or die "Cannot open directory $itraq_dir: $!";
 
-
+my $itraq_count = 0;
 while (my $itraq_file = readdir(iTRAQ_DIR)) {
-	my $filename =$itraq_file;
+	my $pp_count = 0;
+	$itraq_count += 1;
 	if (index($itraq_file, 'tsv') != -1) {
+		# print "iTRAQ file: $itraq_file\n";
+		my $filename =$itraq_file;
 		$filename =~ s/\.tsv//;
-		print "$filename 1st print\n";
+		print "iTRAQ filename: $filename\n";
 		
 		while (my $pp_file = readdir(PP_DIR)) {
-			print "$pp_file 2nd print\n";
-			print "$itraq_file 3rd print\n";
-			
+			# print "Protein Pilot file: $pp_file\n";
+			$pp_count += 1;
+			if (index($pp_file, '_SpectrumSummary.txt') == -1) {
+				print "Not the proper file format. File not parsed.\n";
+				next;
+			}
+
 			my $comp = $pp_file;
-			$comp =~ s/\.tsv//;
-			# print "$comp\n";
+			$comp =~ s/\_SpectrumSummary.txt//;
+			print "Protein Pilot filename: $comp\n";
+			# if ($pp_count == 3) {
+			# 	print "Ending.";
+			# 	last;
+			# }
 			if ($filename eq $comp) {
-				print "Hi\n";
-				print "$comp 4th print\n";
+				print "Same files found.\n";
 				# compare($pp_file, itraq_file);
+				last;
 			}
 			else {
-				print "Here.";
+				print "Here.\n";
 				next;
 			}
 		}
